0b42b2f6672654153141415128d5bfd37a9668fd
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-03-31 16:05:37 -0700

    updating App components and adding redux functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,27 @@
 import React, {Component} from 'react';
+import { connect } from 'react-redux';
+import { addReminder } from '../actions';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+    }
+
+    addReminder() {
+        this.props.addReminder(this.state.text);
+    }
+
     render() {
         return (
             <div className="App">
                 <div className="title">Reminder App</div>
                 <div className="form-inline">
                     <div className="form-group">
-                        <input className="form-control" placeholder="I have to..."/>
-                        <button type="button" className="btn btn-success">Add Reminder</button>
+                        <input onChange={event => this.setState({text: event.target.value})} className="form-control" placeholder="I have to..."/>
+                        <button onClick={() => this.addReminder()} type="button" className="btn btn-success">Add Reminder</button>
                     </div>
                 </div>
             </div>
@@ -16,4 +29,4 @@ class App extends Component {
     }
 }
 
-export default App;
+
--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import { Provider } from 'react-redux';
 import { createStore } from 'redux';
+import reducer from './reducers'
+
+const store = createStore(reducer);
 
 ReactDOM.render(
-    <Provider>
+    <Provider store={store}>
       <App />
     </Provider>,
     document.getElementById('root')
