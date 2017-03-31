5dfe80c8d478d9e68ca1968afe94c484510e36e8
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-03-30 19:33:51 -0700

    updating components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.js';
 import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
 ReactDOM.render(
-    <App />,
+    <Provider>
+      <App />
+    </Provider>,
     document.getElementById('root')
 )
