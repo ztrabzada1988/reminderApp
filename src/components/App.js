fb5686f7cf1cbb8e5dd8b8159bdd9e4e3c9ef984
Zubair Trabzada <ztrabzada@losaltosca.gov>
2017-04-03 16:13:18 -0700

    updating components and redux functionality (adding cookies)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
 import { addReminder, deleteReminder } from '../actions';
+import moment from 'moment';
 
 class App extends Component {
     constructor(props) {
@@ -28,8 +29,11 @@ class App extends Component {
                     reminders.map(reminder => {
                         return (
                             <li key={reminder.id} className="list-group-item">
-                                <div className="list-item">{reminder.text}</div>
-                                <div onClick={() => this.deleteReminder(reminder.id)} className="list-item delete-button">&#x2715;</div>
+                                <div className="list-item">
+                                  <div className="list-item">{reminder.text}</div>
+                                  <div className="list-item"><em>{moment(new Date(reminder.dueDate)).fromNow()}</em></div>
+                                </div>
+                                <div className="list-item delete-button" onClick={() => this.deleteReminder(reminder.id)}>&#x2715;</div>
                             </li>
                         )
                     })
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constants';
+import { bake_cookie, read_cookie } from 'sfcookies'; // saves update in cookies
 
 const reminder = (action) => {
     // ES6 shortcut to replace text: action.text and dueDate: action.dueDate below
@@ -19,13 +20,16 @@ const removeById = (state=[], id) => {
 
 const reminders = (state = [], action) => {
     let reminders = null;
+    state = read_cookie('reminders');
     switch(action.type) {
         case ADD_REMINDER:
             reminders = [...state, reminder(action)]
             console.log('reminders as state', reminders);
+            bake_cookie('reminders', reminders);
             return reminders;
         case DELETE_REMINDER:
             reminders = removeById(state, action.id);
+            bake_cookie('reminders', reminders);
             return reminders
         default:
             return state;
