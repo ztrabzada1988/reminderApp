03dcc70be41269eb2be0f5724e63832ac38402f0
Zubair Trabzada <zubair.trabzada@gmail.com>
2017-04-02 23:28:48 -0700

    updating App components and adding redux functionality

--- src/components/App.js
+++ src/components/App.js
@@ -14,7 +14,39 @@ class App extends Component {
         this.props.addReminder(this.state.text);
     }
 
+    renderReminders() {
+        const { reminders } = this.props;
+        return (
+            <ul className="list-group col-sm-4">
+                {
+                    reminders.map(reminder => {
+                        return (
+                            <li key={reminder.id} className="list-group-item">
+                                <div className="list-item">{reminder.text}</div>
+                                <div className="list-item delete-button">&#x2715;</div>
+                            </li>
+                        )
+                    })
+                }
+            </ul>
+        )
+    }
 
+    render() {
+        return (
+            <div className="App">
+                <div className="title">Reminder App</div>
+                <div className="form-inline reminder-form">
+                    <div className="form-group">
+                        <input onChange={event => this.setState({text: event.target.value})} className="form-control" placeholder="I have to..."/>
+                    </div>
+                    <button onClick={() => this.addReminder()} type="button" className="btn btn-success">Add Reminder</button>
+                </div>
+                    { this.renderReminders() }
+            </div>
+        )
+    }
+}
 
 
 function mapStateToProps(state) {
